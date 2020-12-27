d04704a5745a2422f767e17faec817e2c76633c0
shivanshsinghal107 <shivanshsinghal107@gmail.com>
2020-12-27 16:55:48 +0530

    Uploaded code for backend and frontend

---
+++ frontend/src/components/Room.js
@@ -0,0 +1,123 @@
+import React, { Component } from "react";
+import { Grid, Button, Typography } from "@material-ui/core";
+import CreateRoom from "./CreateRoom";
+
+export default class Room extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            votesToSkip: 2,
+            guestCanPause: false,
+            isHost: false,
+            showSettings: false,
+        };
+        this.roomCode = this.props.match.params.roomCode;
+        this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
+        this.updateShowSettings = this.updateShowSettings.bind(this);
+        this.renderSettingsButton = this.renderSettingsButton.bind(this);
+        this.renderSettings = this.renderSettings.bind(this);
+        this.getRoomDetails = this.getRoomDetails.bind(this);
+        this.getRoomDetails();
+    }
+
+    getRoomDetails() {
+        fetch('/api/get-room' + '?code=' + this.roomCode)
+        .then((response) => {
+            if(!response.ok){
+                this.props.leaveRoomCallback();
+                this.props.history.push("/");
+            }
+            return response.json();
+        })
+        .then((data) => {
+            this.setState({
+                votesToSkip: data.votes_to_skip,
+                guestCanPause: data.guest_can_pause,
+                isHost: data.is_host,
+            });
+        });
+    }
+
+    leaveButtonPressed() {
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+        };
+        fetch('/api/leave-room', requestOptions)
+        .then((_response) => {
+            this.props.leaveRoomCallback();
+            this.props.history.push("/");
+        });
+    }
+
+    updateShowSettings(value) {
+        this.setState({
+            showSettings: value,
+        });
+    }
+
+    renderSettings() {
+        return (
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <CreateRoom
+                        update={true}
+                        votesToSkip={this.state.votesToSkip}
+                        guestCanPause={this.state.guestCanPause}
+                        roomCode={this.state.roomCode}
+                        updateCallback={() => {}}
+                    />
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button variant="contained" color="secondary" onClick={() => this.updateShowSettings(false)}>
+                        Close
+                    </Button>
+                </Grid>
+            </Grid>
+        );
+    }
+
+    renderSettingsButton() {
+        return (
+        <Grid item xs={12} align="center">
+            <Button variant="contained" color="primary" onClick={() => this.updateShowSettings(true)}>
+                Settings
+            </Button>
+        </Grid>
+        );
+    }
+
+    render() {
+        if (this.state.showSettings) {
+            return this.renderSettings();
+        }
+        return <Grid container spacing={1}>
+            <Grid item xs={12} align="center">
+                <Typography variant="h3" component="h3">
+                    Code: {this.roomCode}
+                </Typography>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Typography variant="h6" component="h6">
+                    Gaana Skip Kar Sakte - {this.state.guestCanPause ? "Haan" : "Bhagg"}
+                </Typography>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Typography variant="h6" component="h6">
+                    Gaana Skip Krne Ko - {this.state.votesToSkip} log
+                </Typography>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Typography variant="h6" component="h6">
+                    Host Tu Hai - {this.state.isHost.toString() ? "Haan" : "Nikal"}
+                </Typography>
+            </Grid>
+            {this.state.isHost ? this.renderSettingsButton() : null}
+            <Grid item xs={12} align="center">
+                <Button variant="contained" color="secondary" onClick={this.leaveButtonPressed}>
+                    Leave Room
+                </Button>
+            </Grid>
+        </Grid>
+    }
+}
