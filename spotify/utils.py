e31b018856960853eb4798976c8054368d4b0506
shivanshsinghal107 <shivanshsinghal107@gmail.com>
2021-01-10 19:51:59 +0530

    Added utility functions to execute spotify API request and play/pause song

--- spotify/utils.py
+++ spotify/utils.py
@@ -2,10 +2,14 @@ from .models import SpotifyToken
 from django.utils import timezone
 from datetime import timedelta
 from .credentials import *
-from requests import post
+from requests import post, put, get
+
+BASE_URL = "https://api.spotify.com/v1/me/"
+
 
 def get_user_tokens(session_id):
     user_tokens = SpotifyToken.objects.filter(user = session_id)
+    # print(user_tokens)
     if user_tokens.exists():
         return user_tokens[0]
     else:
@@ -16,6 +20,7 @@ def update_or_create_user_tokens(session_id, access_token, token_type, expires_i
     expires_in = timezone.now() + timedelta(seconds = expires_in)
 
     if tokens:
+        print(f"{access_token}\n{token_type}\n{expires_in}\n{refresh_token}")
         tokens.access_token = access_token
         tokens.token_type = token_type
         tokens.expires_in = expires_in
@@ -36,6 +41,7 @@ def is_spotify_authenticated(session_id):
 
 def refresh_spotify_token(session_id):
     refresh_token = get_user_tokens(session_id).refresh_token
+    # print(f"1: {refresh_token}")
 
     response = post('https://accounts.spotify.com/api/token', data = {
         'grant_type': 'refresh_token',
@@ -44,9 +50,35 @@ def refresh_spotify_token(session_id):
         'client_secret': CLIENT_SECRET
     }).json()
 
+    # print(f"{response}")
+
     access_token = response.get('access_token')
     token_type = response.get('token_type')
     expires_in = response.get('expires_in')
-    refresh_token = response.get('refresh_token')
+    # refresh_token = response.get('refresh_token')
+
+    # print(f"2: {refresh_token}")
 
     update_or_create_user_tokens(session_id, access_token, token_type, expires_in, refresh_token)
+
+def execute_spotify_api_request(session_id, endpoint, post_ = False, put_ = False):
+    tokens = get_user_tokens(session_id)
+    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + tokens.access_token}
+
+    if post_:
+        post(BASE_URL + endpoint, headers = headers)
+    if put_:
+        put(BASE_URL + endpoint, headers = headers)
+
+    response = get(BASE_URL + endpoint, {}, headers = headers)
+
+    try:
+        return response.json()
+    except:
+        return {'error': 'Issue with request'}
+
+def pause_song(session_id):
+    return execute_spotify_api_request(session_id, "player/pause", put_ = True)
+
+def play_song(session_id):
+    return execute_spotify_api_request(session_id, "player/play", put_ = True)
