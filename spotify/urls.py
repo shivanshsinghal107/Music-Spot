20d7e47c89307f016330fb557f8627d312f61df5
shivanshsinghal107 <shivanshsinghal107@gmail.com>
2021-01-11 17:46:47 +0530

    Added urls endpoints for next/previous song

--- spotify/urls.py
+++ spotify/urls.py
@@ -4,8 +4,10 @@ from .views import *
 urlpatterns = [
     path('get-auth-url', AuthURL.as_view()),
     path('redirect', spotify_callback),
-    path('is-authenticated', IsAuthenicated.as_view()),
+    path('is-authenticated', IsAuthenticated.as_view()),
     path('current-song', CurrentSong.as_view()),
     path('pause-song', PauseSong.as_view()),
-    path('play-song', PlaySong.as_view())
+    path('play-song', PlaySong.as_view()),
+    path('next-song', SkipSong.as_view()),
+    path('prev-song', PrevSong.as_view())
 ]
